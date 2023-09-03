502e8e041ab9ad513621d0b308ce4de3c3a7e447
fedora-w <evangeli985107620@gmail.com>
2023-09-03 13:01:39 -0300

    Inserção de usuário com Post

--- src/main/java/com/matheusitech/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheusitech/workshopmongo/resources/UserResource.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,27 +18,33 @@ import com.matheusitech.workshopmongo.dto.UserDTO;
 import com.matheusitech.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	//Retorna lista dos dados do usuarios
+
+	// Retorna lista dos dados do usuarios
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
-		
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<UserDTO> listDTO = service.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
-	
-	//Retorna o usuario pelo o id
-	@GetMapping(value="/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		
+
+	// Retorna o usuario pelo o id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-		
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+
+	//Inserindo o usuáio no banco de dados.
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+	    User obj = service.fromDTO(objDto);
+	    obj = service.insert(obj);
+	    return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+
 }
--- src/main/java/com/matheusitech/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusitech/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusitech.workshopmongo.domain.User;
+import com.matheusitech.workshopmongo.dto.UserDTO;
 import com.matheusitech.workshopmongo.repository.UserRepository;
 import com.matheusitech.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,7 +23,15 @@ public class UserService {
 	
 	//Retorna o usuário pelo o id
 	public User findById(String id) {
-		return repo.findById(id)
-	            .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	//Inserir um usuario no banco de dados.
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return  new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
